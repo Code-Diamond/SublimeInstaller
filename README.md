55093418369d0219fac74b2995d57f1c9dbcf1bf
root <KingJamin@hotmail.com>
2018-10-06 20:55:00 -0500

    Added customizable hotkeys setup

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # SublimeInstaller
-# SublimeInstaller
+This bash script installs sublime text editor, and configures control f1 for window activation.
+Customize ./xbindkeysrc in home directory for more hotkeys. 
+Installs sublime, xdotools, and xkeybinds.
--- installSublime.sh
+++ installSublime.sh
@@ -3,3 +3,12 @@ wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 sudo apt-get update
 sudo apt-get install sublime-text
+
+#Download xbindkeys
+sudo apt-get install -y xbindkeys xdotool
+#bind control F1 to activate sublime
+sudo echo '"xdotool windowactivate $(xdotool search --name "sublime" | grep "." | tail -1);"' >> ~/.xbindkeysrc; 
+sudo echo 'Control + F1' >> ~/.xbindkeysrc;
+#restart xbindkeys
+sudo killall -s1 xbindkeys
+xbindkeys -f ~/.xbindkeysrc 
