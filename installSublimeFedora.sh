db566693addfaa25e3ecdaa06a32eca1dab02daa
JaminROCK <KingJamin@hotmail.com>
2018-10-16 20:05:03 -0500

    Update installSublimeFedora.sh

--- installSublimeFedora.sh
+++ installSublimeFedora.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+#install yakuake
 dnf install yakuake
+#install sublime
 rpm -v --import https://download.sublimetext.com/sublimehq-rpm-pub.gpg
 dnf config-manager --add-repo https://download.sublimetext.com/rpm/stable/x86_64/sublime-text.repo
 dnf install sublime-text
