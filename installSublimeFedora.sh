9cd223cab78f08477abfc430179885f91ad1160a
root <root@j.attlocal.net>
2018-10-16 19:59:47 -0500

    fedora version without xbindkeys

---
+++ installSublimeFedora.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+dnf install yakuake
+rpm -v --import https://download.sublimetext.com/sublimehq-rpm-pub.gpg
+dnf config-manager --add-repo https://download.sublimetext.com/rpm/stable/x86_64/sublime-text.repo
+dnf install sublime-text
